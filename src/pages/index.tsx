40ecdac5fbce736d3e2a27299bf1f8143109c39b
João Pedro FF <joaofrancaf@hotmail.com>
2023-04-03 19:23:45 -0300

    lol

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,32 @@
-import {ContainerStyled} from '../styles/pages/index' 
+import Links from '../components/links'
+import AvatarProfile from '../components/avatarprofile'
+import {ContainerStyled, MainStyled, SideStyled} from '../styles/pages/index' 
+import Tecnologias from '../components/tecnologias'
+import Experiencias from '../components/experiencias'
+import SearchBar from '@/components/searchbar'
+import Card from '../components/card'
 
 export default function Home() {
   return (
+  
     <ContainerStyled>
-      
+
+      <SideStyled>
+        <AvatarProfile/>
+        <Links/>
+        <Tecnologias/>
+        <Experiencias/>
+      </SideStyled>
+
+    
+
+      <MainStyled>
+        <SearchBar/>
+        <Card/>
+        <Card/>
+        <SearchBar/>
+      </MainStyled>
+
     </ContainerStyled>
   )
 }
--- src/styles/pages/index.ts
+++ src/styles/pages/index.ts
@@ -1,5 +1,18 @@
 import {styled} from '../stitches.config'
 
 export const ContainerStyled = styled('div', {
+    margin: '2rem',
     
+})
+
+export const SideStyled = styled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '2.45rem',
+})
+
+export const MainStyled = styled('div', {
+    width: '16rem',
+    display: 'flex',
+    flexWrap: 'nowrap',
 })
